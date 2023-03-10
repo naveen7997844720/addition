f3021955f45f61263baace3d0559ad30f408bbda
naveen7997844720 <127488262+naveen7997844720@users.noreply.github.com>
2023-03-10 16:11:15 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,11 @@
+package com.testautomatiom;
+
+public class addition
+{
+    public static void main(String args[])
+    {
+        int a=10,b=15;
+        int sum= a + b;
+        System.out.println("addition of numbers:" +sum);
+    }
+}
